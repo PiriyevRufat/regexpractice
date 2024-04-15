43837c68abd519097aabb020f66f1121df909643
PiriyevRufat <tu7pj36at@coded.edu.az>
2024-04-15 19:26:21 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+//regex#01
+
+// const regex=new RegExp("[A-Z][a-z]{1,}\\s[A-Z][a-z]{1,}","gm")
+// let numune = prompt("")
+// if (regex.test(numune)) {
+//     console.log(true)    
+//     }
+// else {
+//     console.log(false);
+// }
+
+//regex#02
+
+// const regex=new RegExp("colo[u]?r")
+// let numune="color"
+// if (regex.test(numune)) {
+//         console.log(true)    
+//         }
+//     else {
+//         console.log(false);
+//     }
+
+//regex#03
+
+// const regex=new RegExp("gr[e|a]y")
+// let numune="gray"
+// if (regex.test(numune)) {
+//         console.log(true)    
+//         }
+//     else {
+//         console.log(false);
+//     }
+
+//regex#04
+
+// const regex =new RegExp("^[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\\.[a-zA-Z0-9()]{1,6}\\b([a-zA-Z0-9()_.=]*)$");
+// let numune ="google.com.az.maz";
+
+// if (regex.test(numune)) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//regex#05
+
+// const regex =new RegExp("[1-9][0-9]{3}-([0][1-9]|[1][0-2])-([1-2][0-9]|[0][1-9]|[3][0-1])")
+// let numune ="2004-01-12";
+
+// if (regex.test(numune)) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//regex#06
+
+// const regex =new RegExp("\\(\\+994\\)[\\-\\s]?((070)|(055)|(050)|(077)|(051))[\\-\\s]?\\d{3}[\\-\\s]?\\d{2}[\\-\\s]?\\d{2}");
+// let numune ="(+994)-070-813-98-56";
+
+// if (regex.test(numune)) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
